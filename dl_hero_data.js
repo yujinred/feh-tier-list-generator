9e15bc8ec18d6b2885eb1c176b1bc7074da77617
Eugene Zhang <eugene.zhang8@gmail.com>
2018-10-05 14:29:57 -0400

    Working Refactor
    This is currently able to create a Dragalia Lost tier List.
    
    Details are not flushed out but hey it works.

---
+++ dl_hero_data.js
@@ -0,0 +1,577 @@
+heroes = [
+{
+"id": "100001",
+"name": "Euden",
+"title": "Dragonblood Prince",
+"weaponType": "Sword",
+"rarity": "4",
+"elementalType": "Flame",
+"charaType": "Attack"
+},
+{
+"id": "100002",
+"name": "Elisanne",
+"title": "Anointed Lance",
+"weaponType": "Lance",
+"rarity": "4",
+"elementalType": "Water",
+"charaType": "Support"
+},
+{
+"id": "100003",
+"name": "Ranzal",
+"title": "Arc in the Storm",
+"weaponType": "Axe",
+"rarity": "4",
+"elementalType": "Wind",
+"charaType": "Defense"
+},
+{
+"id": "100004",
+"name": "Cleo",
+"title": "Belle Healer",
+"weaponType": "Staff",
+"rarity": "4",
+"elementalType": "Shadow",
+"charaType": "Healing"
+},
+{
+"id": "100006",
+"name": "Luca",
+"title": "Woodland Archer",
+"weaponType": "Bow",
+"rarity": "4",
+"elementalType": "Light",
+"charaType": "Support"
+},
+{
+"id": "110001",
+"name": "Alain",
+"title": "Captain Craven",
+"weaponType": "Lance",
+"rarity": "3",
+"elementalType": "Flame",
+"charaType": "Defense"
+},
+{
+"id": "110002",
+"name": "Aoi",
+"title": "Impassioned Ninja",
+"weaponType": "Blade",
+"rarity": "3",
+"elementalType": "Flame",
+"charaType": "Attack"
+},
+{
+"id": "110003",
+"name": "Joe",
+"title": "Fire's Fletcher",
+"weaponType": "Bow",
+"rarity": "3",
+"elementalType": "Flame",
+"charaType": "Attack"
+},
+{
+"id": "110004",
+"name": "Xania",
+"title": "Meandering Mage",
+"weaponType": "Wand",
+"rarity": "3",
+"elementalType": "Flame",
+"charaType": "Attack"
+},
+{
+"id": "110005",
+"name": "Renelle",
+"title": "Honorable Treasure Hunter",
+"weaponType": "Dagger",
+"rarity": "3",
+"elementalType": "Flame",
+"charaType": "Support"
+},
+{
+"id": "110006",
+"name": "Aurien",
+"title": "Elegant Gambler",
+"weaponType": "Staff",
+"rarity": "3",
+"elementalType": "Flame",
+"charaType": "Healing"
+},
+{
+"id": "110007",
+"name": "Vanessa",
+"title": "Red-Haired Warrior",
+"weaponType": "Axe",
+"rarity": "4",
+"elementalType": "Flame",
+"charaType": "Attack"
+},
+{
+"id": "110008",
+"name": "Karl",
+"title": "Hero of Justice",
+"weaponType": "Sword",
+"rarity": "4",
+"elementalType": "Flame",
+"charaType": "Support"
+},
+{
+"id": "110010",
+"name": "Mikoto",
+"title": "Nature's Avatar",
+"weaponType": "Blade",
+"rarity": "5",
+"elementalType": "Flame",
+"charaType": "Attack"
+},
+{
+"id": "110011",
+"name": "Ezelith",
+"title": "Genius of the Century",
+"weaponType": "Dagger",
+"rarity": "5",
+"elementalType": "Flame",
+"charaType": "Support"
+},
+{
+"id": "110012",
+"name": "Rex",
+"title": "Charitable Lumberjack",
+"weaponType": "Axe",
+"rarity": "3",
+"elementalType": "Water",
+"charaType": "Attack"
+},
+{
+"id": "110013",
+"name": "Waike",
+"title": "Noble Thief",
+"weaponType": "Bow",
+"rarity": "3",
+"elementalType": "Water",
+"charaType": "Support"
+},
+{
+"id": "110014",
+"name": "Cibella",
+"title": "Staunch Avenger",
+"weaponType": "Lance",
+"rarity": "3",
+"elementalType": "Water",
+"charaType": "Defense"
+},
+{
+"id": "110015",
+"name": "Zardin",
+"title": "Hopeless Narcissist",
+"weaponType": "Sword",
+"rarity": "3",
+"elementalType": "Water",
+"charaType": "Defense"
+},
+{
+"id": "110016",
+"name": "Jurota",
+"title": "Solemn Stalwart",
+"weaponType": "Blade",
+"rarity": "3",
+"elementalType": "Water",
+"charaType": "Attack"
+},
+{
+"id": "110017",
+"name": "Ricardt",
+"title": "Kindly Coward",
+"weaponType": "Staff",
+"rarity": "3",
+"elementalType": "Water",
+"charaType": "Healing"
+},
+{
+"id": "110018",
+"name": "Karina",
+"title": "Free-Spirited Pirate",
+"weaponType": "Axe",
+"rarity": "4",
+"elementalType": "Water",
+"charaType": "Defense"
+},
+{
+"id": "110019",
+"name": "Thaniel",
+"title": "Shifty Sailor",
+"weaponType": "Staff",
+"rarity": "4",
+"elementalType": "Water",
+"charaType": "Healing"
+},
+{
+"id": "110020",
+"name": "Luther",
+"title": "Passionate Dancer",
+"weaponType": "Dagger",
+"rarity": "4",
+"elementalType": "Water",
+"charaType": "Support"
+},
+{
+"id": "110021",
+"name": "Xainfried",
+"title": "Dragon Guardian",
+"weaponType": "Lance",
+"rarity": "5",
+"elementalType": "Water",
+"charaType": "Support"
+},
+{
+"id": "110022",
+"name": "Xander",
+"title": "Born Ruler",
+"weaponType": "Sword",
+"rarity": "5",
+"elementalType": "Water",
+"charaType": "Attack"
+},
+{
+"id": "110023",
+"name": "Johanna",
+"title": "Untamed Warrioress",
+"weaponType": "Axe",
+"rarity": "3",
+"elementalType": "Wind",
+"charaType": "Defense"
+},
+{
+"id": "110024",
+"name": "Sophie",
+"title": "Mana Observer",
+"weaponType": "Staff",
+"rarity": "3",
+"elementalType": "Wind",
+"charaType": "Healing"
+},
+{
+"id": "110025",
+"name": "Nicolas",
+"title": "Walking Encyclopedia",
+"weaponType": "Wand",
+"rarity": "3",
+"elementalType": "Wind",
+"charaType": "Attack"
+},
+{
+"id": "110026",
+"name": "Francesca",
+"title": "Cheerful Merchant",
+"weaponType": "Dagger",
+"rarity": "3",
+"elementalType": "Wind",
+"charaType": "Support"
+},
+{
+"id": "110027",
+"name": "Melody",
+"title": "Menace of a Maid",
+"weaponType": "Blade",
+"rarity": "3",
+"elementalType": "Wind",
+"charaType": "Support"
+},
+{
+"id": "110028",
+"name": "Philia",
+"title": "Love Seeker",
+"weaponType": "Bow",
+"rarity": "3",
+"elementalType": "Wind",
+"charaType": "Attack"
+},
+{
+"id": "110029",
+"name": "Aeleen",
+"title": "Mercurial Lancer",
+"weaponType": "Lance",
+"rarity": "4",
+"elementalType": "Wind",
+"charaType": "Support"
+},
+{
+"id": "110030",
+"name": "Musashi",
+"title": "Wandering Samurai",
+"weaponType": "Blade",
+"rarity": "4",
+"elementalType": "Wind",
+"charaType": "Attack"
+},
+{
+"id": "110031",
+"name": "Eleonora",
+"title": "Curious Archer",
+"weaponType": "Bow",
+"rarity": "4",
+"elementalType": "Wind",
+"charaType": "Attack"
+},
+{
+"id": "110032",
+"name": "Maribelle",
+"title": "Child of the Forest",
+"weaponType": "Wand",
+"rarity": "5",
+"elementalType": "Wind",
+"charaType": "Attack"
+},
+{
+"id": "110033",
+"name": "Linus",
+"title": "Bantam Hero",
+"weaponType": "Axe",
+"rarity": "3",
+"elementalType": "Light",
+"charaType": "Defense"
+},
+{
+"id": "110034",
+"name": "Malka",
+"title": "Golden Armorer",
+"weaponType": "Lance",
+"rarity": "3",
+"elementalType": "Light",
+"charaType": "Support"
+},
+{
+"id": "110035",
+"name": "Raemond",
+"title": "Loyal Servant",
+"weaponType": "Sword",
+"rarity": "3",
+"elementalType": "Light",
+"charaType": "Attack"
+},
+{
+"id": "110036",
+"name": "Malora",
+"title": "Alluring Sniper",
+"weaponType": "Bow",
+"rarity": "3",
+"elementalType": "Light",
+"charaType": "Support"
+},
+{
+"id": "110037",
+"name": "Irfan",
+"title": "Mammonist Mercenary",
+"weaponType": "Dagger",
+"rarity": "3",
+"elementalType": "Light",
+"charaType": "Support"
+},
+{
+"id": "110038",
+"name": "Hope",
+"title": "Knight at Heart",
+"weaponType": "Staff",
+"rarity": "3",
+"elementalType": "Light",
+"charaType": "Healing"
+},
+{
+"id": "110039",
+"name": "Ryozen",
+"title": "Dormant Demon",
+"weaponType": "Lance",
+"rarity": "4",
+"elementalType": "Light",
+"charaType": "Defense"
+},
+{
+"id": "110040",
+"name": "Rawn",
+"title": "Wild Hunter",
+"weaponType": "Bow",
+"rarity": "4",
+"elementalType": "Light",
+"charaType": "Support"
+},
+{
+"id": "110041",
+"name": "Amane",
+"title": "Child of Miracles",
+"weaponType": "Wand",
+"rarity": "4",
+"elementalType": "Light",
+"charaType": "Attack"
+},
+{
+"id": "110042",
+"name": "Julietta",
+"title": "Timorous Paladyn",
+"weaponType": "Axe",
+"rarity": "5",
+"elementalType": "Light",
+"charaType": "Defense"
+},
+{
+"id": "110043",
+"name": "Hildegarde",
+"title": "Devout Oracle",
+"weaponType": "Staff",
+"rarity": "5",
+"elementalType": "Light",
+"charaType": "Healing"
+},
+{
+"id": "110044",
+"name": "Taro",
+"title": "Greenhorn Samurai",
+"weaponType": "Blade",
+"rarity": "3",
+"elementalType": "Shadow",
+"charaType": "Attack"
+},
+{
+"id": "110045",
+"name": "Erik",
+"title": "Feral Noble",
+"weaponType": "Axe",
+"rarity": "3",
+"elementalType": "Shadow",
+"charaType": "Attack"
+},
+{
+"id": "110046",
+"name": "Rodrigo",
+"title": "Sincere Giver",
+"weaponType": "Sword",
+"rarity": "3",
+"elementalType": "Shadow",
+"charaType": "Attack"
+},
+{
+"id": "110047",
+"name": "Althemia",
+"title": "Timid Researcher",
+"weaponType": "Wand",
+"rarity": "3",
+"elementalType": "Shadow",
+"charaType": "Support"
+},
+{
+"id": "110048",
+"name": "Vice",
+"title": "Eager Assassin",
+"weaponType": "Dagger",
+"rarity": "3",
+"elementalType": "Shadow",
+"charaType": "Support"
+},
+{
+"id": "110049",
+"name": "Edward",
+"title": "Perfect Butler",
+"weaponType": "Staff",
+"rarity": "3",
+"elementalType": "Shadow",
+"charaType": "Healing"
+},
+{
+"id": "110050",
+"name": "Berserker",
+"title": "Frenzied Warrior",
+"weaponType": "Sword",
+"rarity": "4",
+"elementalType": "Shadow",
+"charaType": "Defense"
+},
+{
+"id": "110051",
+"name": "Kleimann",
+"title": "Mad Scientist",
+"weaponType": "Wand",
+"rarity": "4",
+"elementalType": "Shadow",
+"charaType": "Support"
+},
+{
+"id": "110052",
+"name": "Orion",
+"title": "Maiden Protector",
+"weaponType": "Dagger",
+"rarity": "4",
+"elementalType": "Shadow",
+"charaType": "Support"
+},
+{
+"id": "110053",
+"name": "Nefaria",
+"title": "Queen of the Sands",
+"weaponType": "Bow",
+"rarity": "5",
+"elementalType": "Shadow",
+"charaType": "Support"
+},
+{
+"id": "110063",
+"name": "Estelle",
+"title": "Loving Preacher",
+"weaponType": "Staff",
+"rarity": "3",
+"elementalType": "Light",
+"charaType": "Healing"
+},
+{
+"id": "110252",
+"name": "Lily",
+"title": "Crystalian Princess",
+"weaponType": "Wand",
+"rarity": "5",
+"elementalType": "Water",
+"charaType": "Attack"
+},
+{
+"id": "110253",
+"name": "Orsem",
+"title": "Dreamer on Ice",
+"weaponType": "Dagger",
+"rarity": "4",
+"elementalType": "Water",
+"charaType": "Attack"
+},
+{
+"id": "110254",
+"name": "Zace",
+"title": "Bone Collector",
+"weaponType": "Lance",
+"rarity": "3",
+"elementalType": "Shadow",
+"charaType": "Attack"
+},
+{
+"id": "110255",
+"name": "Celliera",
+"title": "Hot-Blooded Instructor",
+"weaponType": "Blade",
+"rarity": "4",
+"elementalType": "Water",
+"charaType": "Attack"
+},
+{
+"id": "110267",
+"name": "Hawk",
+"title": "Twilight Hunter",
+"weaponType": "Bow",
+"rarity": "5",
+"elementalType": "Wind",
+"charaType": "Attack"
+},
+{
+"id": "110269",
+"name": "Verica",
+"title": "Guiding Seer",
+"weaponType": "Staff",
+"rarity": "4",
+"elementalType": "Flame",
+"charaType": "Healing"
+}]
